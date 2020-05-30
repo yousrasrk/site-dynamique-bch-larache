b2e6a2bcbc9d20795f50163d7bc9e80231dfb1af
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2020-05-30 22:13:37 +0100

    Add files via upload

---
+++ database/migrations/2019_07_03_164753_create_events_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+           
+            $table->bigIncrements('id');
+            $table->longText('contenu')->nullable();
+            $table->string('filePhoto1')->nullable();
+            $table->string('filePhoto2')->nullable();
+            $table->string('filePhoto3')->nullable();
+            $table->string('filePhoto4')->nullable();
+            $table->string('filePhoto5')->nullable();
+            $table->string('filePhoto6')->nullable();
+            $table->string('fileVideo1')->nullable();
+            $table->string('fileVideo2')->nullable();
+            $table->string('fileVideo3')->nullable();
+            $table->string('fileYoutube1')->nullable();
+            $table->string('fileYoutube2')->nullable();
+            $table->string('Titre')->nullable();
+            $table->dateTime('Date')->nullable();
+            $table->timestamps();
+           
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
